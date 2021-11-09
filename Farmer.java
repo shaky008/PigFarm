f62433a738ca415bacc99018f2cdbe6eb311ce75
shaky008 <sshakya.uutsav@gmail.com>
2021-11-08 16:24:55 -0800

    added few comments and lines

--- Farmer.java
+++ Farmer.java
@@ -8,14 +8,17 @@ public class Farmer {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
-		//variables
+		//Create two different types of feed: apple and potato
 		int appleBins = 0;
 		int potatoBins = 0;
-		
+
+		//Create 3 different pigs
 		Pig pg=new Pig ("Keanu Reeves", "pink", "male", dateofBirth_d(05, 04, 02)); //YY,MM,DD
 		Pig pg2=new Pig ("Tommy", "Green", "male", dateofBirth_d(02, 03, 07));
 		Pig pg3=new Pig ("Scarlett", "Yellow", "Female", dateofBirth_d(01, 01, 07));
 		
+		//Create a user input loop that asks the farmer to enter no of bins they have, ask Y/N to feed the pigs,
+		//if Yes, feed the pigs
 		Scanner sc1 = new Scanner (System.in);
 		
 		String verify = "Y";
@@ -34,6 +37,7 @@ public class Farmer {
 	            fdpg = sc.next();
 	            if (fdpg.equals("Y")||fdpg.equals("y")){
 	                Feed.consume();
+					System.out.println(pg.getName() +", " + pg2.getName() + " and " + pg3.getName() + " ate " +fd1.getFeedName() + " and " + fd.getFeedName());
 	      }
 	            else{
 	                System.out.println("The pigs will not be fed.");

bf58e5590b640d3d42228c57fa8c24033d160e7a
shaky008 <90487676+shaky008@users.noreply.github.com>
2021-11-27 03:53:01 -0800

    updated many lines

--- Feed.java
+++ Feed.java
@@ -1,42 +1,39 @@
 package Farm;
 
 public class Feed {
-
-    //A private static int variable keeping track of the amount of feed that the farmer has in terms of number of bins
     private String feedName;
     private boolean isFull;
     private double weight;
 
-//    getters and setters
+    public boolean isFull() {
+        return this.isFull;
+    }
+
+    public void setFull(boolean full) {
+        this.isFull = full;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
     public String getFeedName() {
-        return feedName;
+        return this.feedName;
     }
 
     public void setFeedName(String feedName) {
         this.feedName = feedName;
     }
 
-    public boolean isFull() {
-        return isFull;
+    public void consume() {
+        this.isFull = false;
     }
 
-    public void setFull(boolean full) {
-        isFull = full;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-    //A constructor that accepts a parameters: A String type for name of feed
     Feed(String feedName_f) {
-        feedName = feedName_f;
-    }
-    // A public method called add which receives an int value and adds that to the number of bins
-    public void add(int addBin) {
-        binCount += addBin;
+        this.feedName = feedName_f;
     }
 }
